dcd0d5fdddcf75b2eecb41b6e0be53637e38111f
zy7y <13271962515@163.com>
2022-11-19 15:30:47 +0800

    chore: 集成PageHelper分页插件

--- src/main/java/com/zy7y/watch_server/controller/UserController.java
+++ src/main/java/com/zy7y/watch_server/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.zy7y.watch_server.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zy7y.watch_server.pojo.User;
 import com.zy7y.watch_server.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 
 @RestController
 public class UserController {
@@ -15,7 +20,9 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/user")
-    public List<User> getUserAll(){
-        return userService.getUserAll();
+    public PageInfo<User> getUserAll(Integer pageNum, Integer pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<User> list =  userService.getUserAll();
+        return new PageInfo<>(list);
     }
 }
