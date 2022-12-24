f4b128c3f2e0e44a8aeb2e03b2fb650459897782
zy7y <13271962515@163.com>
2022-12-24 20:36:49 +0800

    fix: 上传文件保存在项目根目录下upload

--- src/main/java/com/zy7y/watch_server/controller/AsyncFileController.java
+++ src/main/java/com/zy7y/watch_server/controller/AsyncFileController.java
@@ -5,6 +5,7 @@ import com.zy7y.watch_server.pojo.rep.R;
 import com.zy7y.watch_server.service.AsyncFileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import net.lingala.zip4j.ZipFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
+@Slf4j
 @EnableAsync // 异步
 @Tag(name="文件上传")
 @RequestMapping("/upload")
@@ -51,15 +58,23 @@ public class AsyncFileController {
 
     @Operation(summary = "上传并解压", description = "上传zip文件并解压")
     @PostMapping("/unzip")
-    public R<String> unzipFile(MultipartFile file) throws Exception {
-        // 上传文件写入本地
-        File saveFile = new File("/Users/qy_air/Coding/"+ file.getOriginalFilename());
-        Files.write(saveFile.toPath(),file.getBytes());
+    public R<String> unzipFile(MultipartFile file) throws IOException {
+
+        log.info("项目根目录{}", System.getProperty("user.dir"));
+        File dirPath = new File(System.getProperty("user.dir"), "upload");
+        if (!dirPath.exists()){
+            dirPath.mkdir();
+        }
+
+        // 保存文件
+        File saveFilePath = new File(dirPath.getAbsolutePath(), file.getOriginalFilename());
+        file.transferTo(saveFilePath);
+
 
         // 解压本地文件
-        ZipFile zipFile = new ZipFile(saveFile);
-        String path = "/Users/qy_air/Coding/";
-        zipFile.extractAll(path);
+        ZipFile zipFile = new ZipFile(saveFilePath);
+        zipFile.setCharset(StandardCharsets.UTF_8);
+        zipFile.extractAll(dirPath.getAbsolutePath());
         return R.success("处理中...");
     }
 
